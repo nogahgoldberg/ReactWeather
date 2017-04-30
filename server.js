8fe08e53c1ab1851a5734cbcb4ef7cb16cce9ecf
nogahgoldberg <n.goldberg@emergecds.com>
2017-04-30 14:22:47 +0300

    Update middleware for localhost

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var express = require('express');
 
 // Create our app
 var app = express();
+
+// What port Heroku wants to use
 const PORT = process.env.PORT || 3000;
 
+// Redirect https traffic to http- because openWeatherMap only supports http
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
